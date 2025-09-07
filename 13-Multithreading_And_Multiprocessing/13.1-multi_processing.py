1892ff63ae41d8046c2f344900b6fc817ed443a6
AjwarCk <ajwar.trn@outlook.com>
2025-09-07 18:55:29 +0530

    add multi_processing

---
+++ 13-Multithreading_And_Multiprocessing/13.1-multi_processing.py
@@ -0,0 +1,5 @@
+## Process that run in parallel
+## CPU-Bond Tasks-Tasks that are heavy on CPU usage (e.g., mathematical computations, data procesing).
+## Parallel execution - Multiple cores of the CPU
+
+import multiprocessing
