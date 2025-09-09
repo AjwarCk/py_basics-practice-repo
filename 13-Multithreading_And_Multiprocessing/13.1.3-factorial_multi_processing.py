01c7d9a4c78fe6895293e12ce8be57beac2d1d1d
AjwarCk <ajwar.trn@outlook.com>
2025-09-09 18:05:52 +0530

    add factorial using multiprocessing with pool

---
+++ 13-Multithreading_And_Multiprocessing/13.1.3-factorial_multi_processing.py
@@ -0,0 +1,8 @@
+'''
+Real-World Example: Multiprocessing for CPU-bound Tasks
+Scenario: Factorial Calculation
+Factorial Calculations, especially for large numbers,
+Involve significant computational work. Multiprocessing can be used to
+distibute the workload accross multiple CPU cores, improving performance.
+'''
+
