06143b6696dcce22d8039621f0de0ca9926da605
AjwarCk <ajwar.trn@outlook.com>
2025-09-09 18:07:08 +0530

    complete factorial multiprocessing with pool

--- 13-Multithreading_And_Multiprocessing/13.1.3-factorial_multi_processing.py
+++ 13-Multithreading_And_Multiprocessing/13.1.3-factorial_multi_processing.py
@@ -6,3 +6,34 @@ Involve significant computational work. Multiprocessing can be used to
 distibute the workload accross multiple CPU cores, improving performance.
 '''
 
+import time
+import sys
+import math
+import multiprocessing
+
+# Increase the maximum number of digits for integer conversion
+sys.set_int_max_str_digits(100000)
+
+
+# define the function
+
+def computer_factorial(number):
+    print(f"Computing the factorial of {number}")
+    result = math.factorial(number)
+    print(f"Factorial of {number} is {result}")
+    return result
+
+
+if __name__ == '__main__':
+
+    numbers = [500, 600, 700, 800]
+
+    start_time = time.time()
+
+    with multiprocessing.Pool() as pool:
+        results = pool.map(computer_factorial, numbers)
+
+    end_time = time.time()
+
+    print(f"Results: {results}")
+    print(f"The time took: {end_time - start_time} seconds.")
