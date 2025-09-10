3eef1af6152c280d0c7aabef5bdb1cc16b57cd11
AjwarCk <ajwar.trn@outlook.com>
2025-09-10 08:31:35 +0530

    add webscrapping with multithreading

---
+++ 13-Multithreading_And_Multiprocessing/13.2.3-webscrapping_multi_threading.py
@@ -0,0 +1,19 @@
+'''
+Real-world Example: Multithreading for I/O-bound Tasks
+Scenario: Web Scraping
+Web scrapping often involves making numerous network requests to 
+fetch web pages.
+These tasks are I/O-bound because they spend a lot of time waiting 
+for responses from servers.
+Multithreading can significantly improve the performance by allowing 
+multiple web pages to be fetched concurrently.
+'''
+
+'''
+https://python.langchain.com/v0.2/docs/introduction/
+
+https://python.langchain.com/v0.2/docs/concepts/
+
+https://python.langchain.com/v0.2/docs/tutorials/
+'''
+
